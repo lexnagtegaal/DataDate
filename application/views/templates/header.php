cde726f33f1647df300365f0c1eedd0386499319
lexnagtegaal <lex.ook.mail@gmail.com>
2015-04-01 15:23:31 +0200

    header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
-		<link type="text/css" rel="http://www.student.science.uu.nl/~5500206/WT/P3/style/default.css">
-		<script type="text/javascript" src="http://www.student.science.uu.nl/~5500206/WT/P3/script/default.js"></script>
+		<link type="text/css" rel="http://www.students.science.uu.nl/~5500206/WT/P3/style/default.css">
+		<script type="text/javascript" src="http://www.students.science.uu.nl/~5500206/WT/P3/script/default.js"></script>
 	</head>
 	<body>
 	<header>
