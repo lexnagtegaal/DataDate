65ab7f7421a4489c0d16e51e9b6727defc083e5b
Joke95 <j.m.vanede@students.uu.nl>
2015-04-03 20:35:18 +0200

    Stoeien met opmaak

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,18 +1,25 @@
 <html>
 	<head>
-		<link type="text/css" rel="http://www.students.science.uu.nl/~4291247/webtech2/Joke/wp3/style/default.css">
+		<link type="text/css" rel="stylesheet" href="http://www.students.science.uu.nl/~4291247/webtech2/Joke/wp3/style/default.css">
 		<script type="text/javascript" src="http://www.students.science.uu.nl/~4291247/webtech2/Joke/wp3/script/default.js"></script>
+		
 		<title><?php echo $title ?></title>
 	</head>
 	<body>
 	<header>
+		<div id="logo" class="margin">
+		<a href="home" >
+			<img src="www.students.science.uu.nl/~4291247/webtech2/Joke/wp3/image/Blijemensen.png">
+		</a>
+		</div>
 		<nav>
 			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">Over ons</a></li>
+				<li class="margin"><a href="home">Home</a></li>
+				<li><a href="about">Over ons</a></li>
 				<li><a href="">Registeren</a></li>
 				<li><a href="">Zoeken</a></li>
 			</ul>
 		</nav>
 		
 	</header>
+<div id="content" class="margin">
