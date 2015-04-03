90ddd942b0af592fc7951820e4e7339b55543c07
Joke95 <j.m.vanede@students.uu.nl>
2015-04-03 20:36:02 +0200

    stoeien me opmaak

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,13 @@
-<p>Hallo Wereld</p>
+<h1>Home</h1>
+
+<div class = "profiles">
+	<div>profiel 1</div>
+	<div>profiel 2</div>
+	<div>profiel 3</div>
+	<div>profiel 4</div>
+	<div>profiel 5</div>
+	<div>profiel 6</div>
+</div>	
+<div id = "inloggen">
+	inloggen
+</div>
