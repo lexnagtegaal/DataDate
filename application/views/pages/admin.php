59b8d083510940a62ecaeb0d45d904d96cd26d0d
lexnagtegaal <lex.ook.mail@gmail.com>
2015-04-19 20:50:39 +0200

    admin nog niet af

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -1,4 +1,23 @@
 <?php
 echo form_open("Admin","id='admin'");
+echo heading("Afstandsmaat",2);
+echo form_label("Methode van Dice","D1");
+
+echo form_label("Methode van Jacard","D2");
+
+echo form_label("Methode van Cosinus","D3");
+
+echo form_label("Methode van Overlapping","D4");
+
+echo heading("X-factor",2);
+echo form_label("Sterkte Persoonlijkheidstype ten opzichte van Merken.");
+?>
+<p>1 sluit merken matching, 0 sluit Persoonlijkheidstype uit.</p>
+<?php
+echo heading("Alfa-factor",2);
+echo form_label("Sterkte waarmee nieuwe likes de persoonlijkheidsvoorkeur verandert");
+?>
+<p>1 sluit verandering uit, 0 zorgt voor totale verandering</p>
+<?php 
 echo form_close();
 ?>
