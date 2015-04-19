5937d6a718174d679b5e5952fe81177ce1c15417
lexnagtegaal <lex.ook.mail@gmail.com>
2015-04-19 17:09:14 +0200

    TABEL.JS
    Layout html niveau gefixt

--- script/tabel.js
+++ script/tabel.js
@@ -1,148 +1,129 @@
+function createTR(object,attributen){
+	//data = array met attributen in key : value paren.
+	//object moet ook een echt object zijn, bijvoorbeeld textNode!
+	TR=document.createElement('tr');
+	if(attributen!=undefined){
+		$(TR).attr(attributen); // bijvoorbeeld <th id='profielfoto'></tr>
+	}
+	if(object!=undefined){
+		$(TR).append(object);	
+	}
+	return TR;
+}
+function createTH(object,attributen){
+	//data = array met attributen in key : value paren.
+	//object moet ook een echt object zijn, bijvoorbeeld textNode!
+	TH=document.createElement('th');
+	if(attributen!=undefined){
+		$(TH).attr(attributen); // bijvoorbeeld <th id='profielfoto'></tr>
+	}
+	if(object!=undefined){
+		$(TH).append(object);	
+	}
+	return TH;
+}
+function createImg(attributen){
+	//data = array met attributen in key : value paren.
+	return $(document.createElement('img')).attr(attributen); // bijvoorbeeld <img  src='image/Man.png'/>
+}
+function createLink(object,attributen){
+	//data = array met attributen in key : value paren.
+	//object moet ook een echt object zijn, bijvoorbeeld textNode!
+	LINK=document.createElement('a');
+	if(attributen!=undefined){
+		$(LINK).attr(attributen); // bijvoorbeeld <th id='profielfoto'></tr>
+	}
+	if(object!=undefined){
+		$(LINK).append(object);	
+	}
+	return LINK;
+}
+
+/* Het idee is steeds vanuit de kleinste object uit te werken 
+ * en dat object toe te voegen aan de createTR/TH/Link functie
+ */
 function createTable($data){
 
-	var $Tabel=document.createElement('table'); // geeft <tabel></tabel>
-	if($data['Foto']!==undefined){
-		$Foto = $(document.createElement("img"))
-					.attr({
-							src: $data['Foto'],
-							alt: $data['Bijnaam']
-						});
-		$TR=$(document.createElement('tr')).attr('id','profielfoto'); // geeft <tr id='profielfoto'></tr>
-		$TH=document.createElement('th');
-		if($data['Overzicht']){
-			$($TH).append(
-						$(document.createElement('a'))
-							.attr("href", "Profilepage/show/"+$data['Bijnaam']+"")
-							.append($Foto)
-							);
-		}else{
-			$($TH).append($Foto);
-		}
-		$($TR).append($TH);
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Bijnaam']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-			$(document.createElement('th'))
-				.append(document.createTextNode("Bijnaam:"))); // voegt <th>Bijnaam:</th> toe.
-		$TH=document.createElement('th');
-		if($data['Overzicht']){
-			$($TH).append(
-					$(document.createElement('a'))
-						.attr("href", "Profilepage/show/"+$data['Bijnaam']+"")
-						.append($data['Bijnaam'])
-						);
-		}else{
-			$($TH).append($data['Bijnaam']);
-		}
-		$($TR).append($TH);
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Geslacht']!==undefined){
-		$TR=$(document.createElement('tr'))
-				.attr('class','tekst');
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(
-							document.createTextNode("Geslacht:"))); // voegt <th>Geslacht:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(
-							document.createTextNode($data['Geslacht']))); // voegt bijvoorbeeld <th>Man</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Geboortedatum']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode("Geboortedatum:"))); // voegt <th>Geboortedatum:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Geboortedatum']))); // voegt bijvoorbeeld <th>Geboortedatum</th> toe.
-		$($($Tabel).append($TR)); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Leeftijd']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode("Leeftijd:"))); // voegt <th>Leeftijd:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Leeftijd']))); // voegt bijvoorbeeld <th>26</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Beschrijving']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode("Beschrijving:"))); // voegt <th>Beschrijving:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Beschrijving']))); // voegt bijvoorbeeld <th>Hoi ik ben Lex en ik hou van meisjes.</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Geslachtsvoorkeur']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th')).
-					append(document.createTextNode("Geinteresseerd in:"))); // voegt <th>Geintereseerd in:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Geslachtsvoorkeur']))); // voegt bijvoorbeeld <th>Vrouwen</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Minimumleeftijd']!==undefined && $data['Maximumleeftijd']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode("tussen de leeftijd"))); // voegt <th>tussen de leeftijd</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Minimumleeftijd'] + " en " + $data['Maxmimumleeftijd']))); // voegt bijvoorbeeld <th>19 en 27</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Persoonlijkheidstype']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode("Persoonlijkheidstype"))); // voegt <th>Persoonlijkheidstype:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Persoonlijkheidstype']))); // voegt bijvoorbeeld <th>Extravert</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Persoonlijkheidsvoorkeur']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode("Persoonlijkheidsvoorkeur:"))); // voegt <th>Persoonlijkheidsvoorkeur:</th> toe.
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($data['Persoonlijkheidsvoorkeur']))); // voegt bijvoorbeeld <th>Introvert</th> toe.
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	if($data['Merk']!==undefined){
-		$TR=$(document.createElement('tr')).attr('class','tekst'); // geeft <tr class='tekst'></tr>
-		$($TR).append(
-				$(document.createElement('th')).
-					append(document.createTextNode("Merken:"))); // voegt <th>Merken:</th> toe.
-		$merken="";
-		$.ajax({
-			url: 'Profile/brand/'+$data['Bijnaam']+'/'+$data['Merk'],
-			type: 'GET', 
-			dataType: 'xml',
-		    success: function(returnedXMLResponse){
-		    	$('user', returnedXMLResponse).each(function(){
-		    		$merk = $('Merk',this).text();
-		    		$($merken).append("<span class='merk'>"+$merk+"</span>"); // hierdoor ontstaat een opsomming van bijvoorbeeld <span class"merk">Coca-Cola</span>
-		            	  })
-		    	}
+	var Type=$data['Persoonlijkheidstype']; // aanpassen nog!
+	var Tabel=document.createElement('table'); // geeft <tabel></tabel>
+	
+	//een moeder TR voor alle TR en TH's binnen de eerste TR
+	var TR_parent=createTR(undefined,{
+										'id'	:	'Profielfoto'
+										}); // <tr id='Profielfoto'></tr>
+		
+	//eerst maken de img klaar.
+	var Foto=createImg({
+			'src': $data['Foto'],
+			'alt': $data['Bijnaam']
 			});
-		$($TR).append(
-				$(document.createElement('th'))
-					.append(document.createTextNode($merken)));
-		$($Tabel).append($TR); // voegt het gehele TR element toe aan tabel.
-	}
-	return $Tabel;
+	var Profielfoto=createLink(Foto,{
+									'href':'Profielfoto/unique/'+$data['Bijnaam']
+									});
+	var TH=createTH(Profielfoto,undefined); // <th>$profielfoto</th>
+	$(TR_parent).append(TH); // <tr id='Profielfoto>$TH</tr>
+	
+	// De tweede TH aanmaken voor toevoeging van 3 TRs
+	TH=createTH(undefined,undefined); // <th></th>
+	
+	//Eerst de Bijnaam toevoegen
+	var TH_inside=createTH(document.createTextNode($data['Bijnaam']),undefined);
+	var TR_inside=createTR(TH_inside,undefined); // <tr><th>$bijnaam</th></tr>
+	$(TH).append(TR_inside);
+	
+	//Het geslacht toevoegen
+	TH_inside=createTH(document.createTextNode($data['Geslacht']),undefined);
+	TR_inside=createTR(TH_inside,undefined); // <tr><th>$geslacht</th></tr>
+	$(TH).append(TR_inside);
+	
+	//Leeftijd toevoegen
+	TH_inside=createTH(document.createTextNode($data['Leeftijd']),undefined);
+	TR_inside=createTR(TH_inside,undefined); // <tr><th>$leeftijd</th></tr>
+	$(TH).append(TR_inside);
+	
+	//De tweede TH is gereed voor toevoeging aan de TRparent
+	$(TR_parent).append(TH);
+	//Nu de eerste TR_parent af is... toevoegen aan Tabel
+	$(Tabel).append(TR_parent);
+	
+	//De eerste TR is nu af!
+	
+	//Beschrijving
+	TR_parent=createTR(undefined,undefined);
+		TH=createTH(document.createTextNode("Beschrijving:"),undefined);
+		$(TR_parent).append(TH);
+		TH=createTH(document.createTextNode($data['Beschrijving']),undefined);
+		$(TR_parent).append(TH);
+	$(Tabel).append(TR_parent);
+	
+	//Persoonlijkheidstype
+	TR_parent=createTR(undefined,undefined);
+		TH=createTH(document.createTextNode("Type:"),undefined);
+		$(TR_parent).append(TH);
+		TH=createTH(document.createTextNode(Type),undefined);
+		$(TR_parent).append(TH);
+	$(Tabel).append(TR_parent);
+
+	//Beschrijving
+	
+	var Merken="";
+	$.ajax({
+		url: 'Profile/brand/'+$data['Bijnaam'],
+		type: 'GET', 
+		dataType: 'xml',
+	    success: function(returnedXMLResponse){
+	    	$('user', returnedXMLResponse).each(function(){
+	    		var Merk = $('Merk',this).text();
+	    		$(Merken).append("<span class='merk'>"+Merk+"</span>"); // hierdoor ontstaat een opsomming van bijvoorbeeld <span class"merk">Coca-Cola</span>
+	            	  })
+	    	}
+		});
+
+	TR_parent=createTR(undefined,undefined);
+		TH=createTH(document.createTextNode("Merken:"),undefined);
+		$(TR_parent).append(TH);
+		TH=createTH(document.createTextNode(Merken),undefined);
+		$(TR_parent).append(TH);
+	$(Tabel).append(TR_parent);
+return Tabel;
 }
