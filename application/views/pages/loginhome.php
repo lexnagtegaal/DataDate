b471a9ca3ab46bddaa79884ab3e7f298cdeac389
Joke95 <j.m.vanede@students.uu.nl>
2015-04-20 11:30:58 +0200

    random op inloghome

--- application/views/pages/loginhome.php
+++ application/views/pages/loginhome.php
@@ -1,7 +1,7 @@
 <div class = "profiles">
 <h1 class="title" >Home</h1>
 
-<div class="matches">
+<div class="random">
 <p>De profielen worden geladen. Een ogenblik geduld a.u.b.</p>
 </div>
 <a class="java" href="rndm">meer</a>
