55740d82a7d99a1b6c0e816428e4c46763cf1272
lexnagtegaal <lex.ook.mail@gmail.com>
2015-04-20 03:53:19 +0200

    Profielweergave gedaan
    Inclusief ready to test script voor like matching!

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -98,15 +98,54 @@ class Profile extends MY_Controller{
 		 */
 	}
 	public function unique($username="username"){
-		$data=$this->matching->get_user($username); // haalt met behulp van matching_model.php alle data op van de profielweergave
-		if(!$data){
+		
+		if(!($this->matching->get_user($username))){ // haalt met behulp van matching_model.php alle data op van de profielweergave)){
 			redirect("home");
 		}else{
+			
+			$this->form_validation->set_rules('email', 'E-mailadres', 'trim|required|callback_email_check');
+			$this->form_validation->set_rules('FirstName','Voornaam', 'trim|required');
+			$this->form_validation->set_rules('LastName','Achternaam', 'trim|required');
+			$this->form_validation->set_rules('Beschrijving','Beschrijving','required');
+			$this->form_validation->set_rules('Voorkeur[]','Interresse','required');
+			$this->form_validation->set_rules('min','Mimimumleeftijd','required|callback_minimaxi_check['.set_value('max').']');
+			$this->form_validation->set_rules('max','Maximumleeftijd','required');
+			if($this->form_validation->run()==TRUE){
+				$updaten = array(
+						'Voornaam'			=> strtolower(set_value('FirstName')),
+						'Tussenvoegsel'		=> strtolower(set_value('MiddleName')),
+						'Achternaam'		=> strtolower(set_value('LastName')),
+						'E-mailadres'		=> strtolower(set_value('email')),
+						'Beschrijving'		=> set_value('Beschrijving'),
+						'Geslachtsvoorkeur'	=> set_value('Voorkeur[0]').set_value('Voorkeur[1])'), // resulteert in ManVrouw.
+						'Minimumleeftijd'	=> (set_value('min')+18),
+						'Maximumleeftijd' 	=> (set_value('max')+18)
+						//'Merken'			=>array(set_value('merken[]'))
+				);
+				$merk_array=set_value('merken[]');
+				$this->db->where('Bijnaam',$this->session->userdata('username'));
+				$this->db->update('Gebruikersprofiel',$updaten);
+				$this->db->where('Bijnaam',$this->session->userdata('username'));
+				$this->db->delete('Merk');
+				print_r($merk_array);
+				for($i=0;$i<count($merk_array);$i++){
+					$merken= array(
+							'Bijnaam'			=> $this->session->userdata('username'),
+							'Merk'				=> $merk_array[$i]
+					);
+					$this->db->insert('Merk',$merken);
+				}
+			}
+			$data=$this->matching->get_user($username); // haalt met behulp van matching_model.php alle data op van de profielweergave
 			$data['owner']=($username==$this->session->userdata('username'));
 			$data['Merken']=($this->matching->get_brands($this->session->userdata('username')));
 			$data['mailadres']=$data['E-mailadres'];
+			$data['match']=($this->matching->is_match($username));
 			$this->view('profilepage',$data);
+				
 		}
+		
+		
 	}
 	public function show($query){
 		header ("Content-Type:application/xml");
@@ -122,6 +161,32 @@ class Profile extends MY_Controller{
 				'tab'           => "\t"
 		);
 		echo $this->dbutil->xml_from_result($query, $config);
+	}
+	
+	public function email_check($email)
+	{
+		// controleert of gebruikersnaam voorkomt in de tabel.
+		//opbouw van de query'
+		$this->db->select('*');
+		$this->db->from('Gebruiker');
+		$this->db->where('Email',strtolower($email));
+		$query=$this->db->get();// SELECT 'Email' from Gebruiker where 'Email'=$email;
+		$result=$query->row_array();
+		if ($query->num_rows() > 0 && $result['Bijnaam']!=$this->session->userdata('username')){
+			$this->form_validation->set_message('email_check', '{field} bestaat al in de database.');
+			return FALSE;
+		}else{
+			return TRUE;
+		}
+	}
+	
+	public function minimaxi_check($min,$max){
+		if($min>$max){
+			$this->form_validation->set_message('minimaxi_check', '{field} moet kleiner zijn dan de maximumleeftijd.');
+			return FALSE;
+		}else{
+			return TRUE;
+		}
 	}
 }
 ?>
